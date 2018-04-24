b6c119c24b22fd607871cf4a111d1464cbb8f7e5
CesarGeek <cesar.work10@gmail.com>
2018-04-23 22:53:46 -0300

    Atividade sobre a Separação de Texto

---
+++ LetrasComTraco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LetrasComTraco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            string texto;
+            Console.Clear();
+
+            Console.WriteLine("Olá, Bem Vindo ao Separador de Texto");
+            Console.WriteLine("Digite um Texto");
+            texto = Console.ReadLine();
+            for(int i = 0 ; i <  texto.Length; i ++)
+                Console.Write(texto[i]+"-");
+
+        }
+    }
+}
